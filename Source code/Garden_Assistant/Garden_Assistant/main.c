729446481c461a9dc0d61ebb60b8b26db523a202
Phạm Hữu Thọ <36005079+Phamthous@users.noreply.github.com>
2020-05-26 16:51:58 +0700

    update

---
+++ Source code/Garden_Assistant/Garden_Assistant/main.c
@@ -0,0 +1,32 @@
+/*
+ * Garden_Assistant.c
+ *
+ * Created: 20/05/2020 4:50:29 AM
+ * Author : HCMUS-VL-VLKT
+ */ 
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <lcd.h>
+#include <pmw.h>
+#include <DHT.h>
+#include <UART.h>
+#include <SPI.h>
+#include <I2C.h>
+
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+    }
+}
+
