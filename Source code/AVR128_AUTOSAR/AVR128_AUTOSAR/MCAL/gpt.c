6d1e76a82133e279cd6e979d68aa9f1368a8d627
ThoPH <phamhuutho321@gmail.com>
2022-08-07 23:03:49 +0700

    delete

--- Source code/AVR128_AUTOSAR/AVR128_AUTOSAR/MCAL/gpt.c
+++
@@ -1,8 +0,0 @@
-/*
- * gpt.c
- *
- * Created: 7/30/2022 3:58:20 PM
- *  Author: ThoPH
- */ 
-
-#include <gpt.h>
--- Source code/AVR128_AUTOSAR/AVR128_AUTOSAR/MCAL/spi.c
+++
@@ -1,8 +0,0 @@
-/*
- * spi.c
- *
- * Created: 7/30/2022 4:00:14 PM
- *  Author: ThoPH
- */ 
-
-#include "spi.h"
